b7e643ff523402a7e91db7e10a99b989102e0546
Sectimus <Sectimus@hotmail.co.uk>
2020-07-24 11:39:11 +0100

    build

---
+++ precache-manifest.cdff7b483795bbce4e3a5a3cd75790ec.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15a1e9b67168b8253539f809e1ac34eb",
+    "url": "/macromancer/index.html"
+  },
+  {
+    "revision": "57ff6788c9c2cff0814b",
+    "url": "/macromancer/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "9c9a7e1c07693ab65f36",
+    "url": "/macromancer/static/css/main.f27c22ef.chunk.css"
+  },
+  {
+    "revision": "57ff6788c9c2cff0814b",
+    "url": "/macromancer/static/js/2.dda7a805.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/macromancer/static/js/2.dda7a805.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c9a7e1c07693ab65f36",
+    "url": "/macromancer/static/js/main.79b6402d.chunk.js"
+  },
+  {
+    "revision": "cbb3fcb7eb63f231a2a6",
+    "url": "/macromancer/static/js/runtime-main.1e3bef05.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/macromancer/static/media/logo.ee7cd8ed.svg"
+  }
+]);
